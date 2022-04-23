ea21ab277a682acf3e954395c0b7f096ffae3de1
Bruno LE LAY <brunolelay2000@gmail.com>
2022-04-23 14:06:27 +0200

    model correctly done

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :overview, presence: true
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+end
